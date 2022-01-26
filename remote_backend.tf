a8ef59ffc76d89abb238dac1378c2ae77de4e4f4
xavi <xavileci@gmail.com>
2022-01-26 05:44:54 +0000

    fix: conflicts on configuration

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,11 +1,7 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-<<<<<<< HEAD
     organization = "xavileci-training"
-=======
-    organization = "xavileci-trining"
->>>>>>> 4ad62990f4af2a08c9b074bac69bd49a78e8346e
     workspaces {
       name = "hashicat-azure"
     }
