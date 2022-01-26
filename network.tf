3079d38df98718851476502c36480bf1b222ed62
xavi <xavileci@gmail.com>
2022-01-26 07:37:32 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 module "network" {
   source              = "Azure/network/azurerm"
-  resource_group_name = azurerm_resource_group.xavileci.resource_group_name
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
@@ -16,5 +16,5 @@ module "network" {
     costcenter  = "it"
   }
 
-  depends_on = [azurerm_resource_group.xavileci]
+  depends_on = [azurerm_resource_group.myresourcegroup]
 }
